a6176235dadce02fd70092a0f766f0eaf77e4ff1
Varun Kamath <mail.varunkamath@gmail.com>
2024-03-27 16:20:24 +0530

    Initial commit

---
+++ server/customer/index.js
@@ -0,0 +1,20 @@
+// Simple express server which returns working server on '/'
+
+const cors = require("cors");
+const express = require("express");
+const app = express();
+
+const PORT = 8001;
+
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Customer Server is working" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Customer server is running on http://localhost:${PORT}`);
+});
